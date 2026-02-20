89801bd871177955a1f9368ed8a42670e80ad7e8
Max Esnée <contact@max-esnee.com>
2026-02-20 14:03:39 +0100

    Fix text block when there's no title

--- app/components/BlockText.vue
+++ app/components/BlockText.vue
@@ -1,8 +1,9 @@
 <template>
 	<section class="cols">
-		<div class="col col-text col-with-padding">
-			<h3 class="h2">{{ props.block.content.title }}</h3>
+		<div v-if="props.block.content.titre" class="col col-text col-with-padding">
+			<h3 class="h2">{{ props.block.content.titre }}</h3>
 		</div>
+		<div v-else class="col grid-background"></div>
 		<div class="col col-text col-with-padding">
 			<div class="text" v-html="props.block.content.text"></div>
 		</div>
