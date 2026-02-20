6a110c6ec1767d3931ac16703fca053b46dd8cb7
Max Esnée <contact@max-esnee.com>
2026-02-20 09:55:24 +0100

    Add conditional logic for project intro

--- app/pages/projets/[slug].vue
+++ app/pages/projets/[slug].vue
@@ -1,41 +1,43 @@
 <template>
-	<Head>
-		<Title>{{ siteTitle }} | {{ data?.result.title }}</Title>
-	</Head>
-	<main>
-		<header class="project-header">
-			<div class="project-header-title">
-				<h1 class="h1">{{ data?.result.title }}</h1>
-			</div>
-			<img :src="data?.result.image_cover.url" :alt="data?.result.image_cover.alt" class="project-header-image">
-		</header>
-		<section class="project-intro cols cols-align-start">
-			<div>
-				<div class="col col-text col-with-padding">
-					<h2 class="h2 purple">En bref</h2>
-					<p>{{ data?.result.caption }}</p>
+	<div v-if="project">
+		<Head>
+			<Title>{{ siteTitle }} | {{ project.title }}</Title>
+		</Head>
+		<main>
+			<header class="project-header">
+				<div class="project-header-title">
+					<h1 class="h1">{{ project.title }}</h1>
 				</div>
-				<div class="col col-text col-with-padding">
-					<h2 class="h2 purple">Intentions</h2>
-					<div class="text" v-html="data?.result.intention"></div>
+				<img :src="project.image_cover.url" :alt="project.image_cover.alt" class="project-header-image">
+			</header>
+			<section v-if="hasIntro" class="project-intro cols cols-align-start">
+				<div>
+					<div v-if="project.caption" class="col col-text col-with-padding">
+						<h2 class="h2 purple">En bref</h2>
+						<p>{{ project.caption }}</p>
+					</div>
+					<div v-if="project.intention"class="col col-text col-with-padding">
+						<h2 class="h2 purple">Intentions</h2>
+						<div class="text" v-html="project.intention"></div>
+					</div>
 				</div>
+				<div v-if="project.description"class="col col-text col-with-padding">
+					<ul class="tag-list">
+						<li v-for="tag in project.tags" class="tag">{{ tag.title }}</li>
+					</ul>
+					<div class="text" v-html="project.description"></div>
+				</div>
+			</section>
+
+			<hr class="divider">
+
+			<div class="blocks">
+				<Blocks :content="project.content || []" />
 			</div>
-			<div class="col col-text col-with-padding">
-				<ul class="tag-list">
-					<li v-for="tag in data?.result.tags" class="tag">{{ tag.title }}</li>
-				</ul>
-				<div class="text" v-html="data?.result.description"></div>
-			</div>
-		</section>
 
-		<hr class="divider">
-
-		<div class="blocks">
-			<Blocks :content="data?.result.content || []" />
-		</div>
-
-		<hr class="divider">
-	</main>
+			<hr class="divider">
+		</main>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -108,5 +110,11 @@
 				},
 			}
 		}
+	});
+
+	const project = computed(() => data.value?.result);
+
+	const hasIntro = computed(() => {
+		return !!(project.value?.caption || project.value?.intention || project.value?.description);
 	});
 </script>
