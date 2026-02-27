ab779ce579128ec06d32299605a3bb02272cbb72
Max Esnée <contact@max-esnee.com>
2026-02-27 14:40:45 +0100

    Add max-height for images in image block

--- app/components/BlockImage.vue
+++ app/components/BlockImage.vue
@@ -1,7 +1,9 @@
 <template>
 	<section v-if="props.block.content.image" class="section-image">
 		<figure>
-			<img :src="props.block.content.image[0].url" :alt="props.block.content.image[0].alt" :width="props.block.content.image[0].width" :height="props.block.content.image[0].height">
+			<div class="image-container">
+				<img :src="props.block.content.image[0].url" :alt="props.block.content.image[0].alt" :width="props.block.content.image[0].width" :height="props.block.content.image[0].height">
+			</div>
 			<figcaption v-if="props.block.content.caption || props.block.content.credits" class="section-caption">
 				<div v-if="props.block.content.caption" class="text small" v-html="props.block.content.caption"></div>
 				<div v-if="props.block.content.credits" class="text x-small mono" v-html="props.block.content.credits"></div>
