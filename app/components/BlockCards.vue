54b0c08d529d8ba43d3218c7b8377219cae463e8
Max Esnée <contact@max-esnee.com>
2026-02-27 18:03:44 +0100

    Card block: add arrows, random rotations and animation

--- app/components/BlockCards.vue
+++ app/components/BlockCards.vue
@@ -5,23 +5,51 @@
 		</header>
 
 		<ul v-if="props.block.content.cards.length" class="card-list grid-background">
-			<li v-for="card in props.block.content.cards" class="card" v-if="props.block.content.style == 'light'">
-				<p class="small mono" v-html="card.text"></p>
-				<p v-if="card.baseline" class="x-small mono">{{ card.baseline }}</p>
-			</li>
-
-			<li v-for="card in props.block.content.cards" class="card card-purple" v-if="props.block.content.style == 'color'">
-				<p class="mono" v-html="card.text"></p>
-				<p v-if="card.baseline" class="x-small mono">{{ card.baseline }}</p>
-			</li>
+			<template v-for="(card, i) in props.block.content.cards">
+				<div class="card-container">
+					<li class="card" :class="{'card-purple': isColorStyle}" :style="getAngles()">
+						<p class="mono" :class="{'small': !isColorStyle}" v-html="card.text"></p>
+						<p v-if="card.baseline" class="x-small mono">{{ card.baseline }}</p>
+					</li>
+					<div v-if="i < props.block.content.cards.length-1" class="card-arrow" :class="{top: !isEven(i), bottom: isEven(i)}" v-html="Arrow"></div>
+				</div>
+			</template>
 		</ul>
 	</section>
 </template>
 
 <script setup lang="ts">
 	import type Block from "#shared/cms_api";
+	import Arrow from '~/assets/svg/arrow.svg?raw';
 
 	const props = defineProps<{
 		block: Block
 	}>();
+
+	const isColorStyle = props.block?.content.style == 'color';
+
+	function isEven(i) {
+		return i%2 == 0;
+	}
+
+	function getAngles() {
+		const base = (Math.random() - 0.5) * 30;
+		const hover = base - (Math.sign(base) * 8);
+
+		return {
+			'--base-angle': `${base}deg`,
+			'--hover-angle': `${hover}deg`
+		};
+	};
 </script>
+
+<style scoped>
+	.card {
+		transform: rotate(var(--base-angle));
+		transition: transform 0.3s ease-out;
+
+		&:hover {
+			transform: rotate(var(--hover-angle)) scale(1.02);
+		}
+	}
+</style>
