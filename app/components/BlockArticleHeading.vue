d9a26323953a7f3ad6b5fbab1602a5c2f6b94c3d
Max Esnée <contact@max-esnee.com>
2026-04-17 15:21:59 +0200

    Article Heading Block: hide title column when there is no title

--- app/components/BlockArticleHeading.vue
+++ app/components/BlockArticleHeading.vue
@@ -1,8 +1,9 @@
 <template>
 	<section class="section-article-heading cols" :class="props.block.content.text.length > 500 ? 'cols-align-start' : ''">
-		<header class="col col-text col-with-padding">
+		<header v-if="props.block.content.titre" class="col col-text col-with-padding">
 			<h2 class="h2 purple" v-html="props.block.content.titre"></h2>
 		</header>
+		<div v-else class="grid-background"></div>
 		<div class="col col-text col-with-padding">
 			<div class="text" v-html="props.block.content.text"></div>
 		</div>
