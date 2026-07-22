6eb9410eb08bb56b221ad1d7b424925f7698c239
Max Esnée <contact@max-esnee.com>
2026-07-22 13:30:36 +0200

    Retrieve resolved video url object from backend and accept youtube, vimeo and direct url

--- app/components/BlockVideo.vue
+++ app/components/BlockVideo.vue
@@ -5,8 +5,25 @@
 		</header>
 
 		<figure>
-			<div v-if="embedUrl" class="video-container">
-				<iframe width="560" height="315" :src="embedUrl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+			<div v-if="embed" class="video-container">
+				<iframe 
+					v-if="embed.provider !== 'file'"
+					width="560" height="315" 
+					:src="embed.src" 
+					title="Video player" 
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+					referrerpolicy="strict-origin-when-cross-origin" 
+					allowfullscreen>
+				</iframe>
+				<video
+					v-else
+					:src="embed.src"
+					controls
+					playsinline
+					preload="metadata"
+				></video>
+
 			</div>
 
 			<figcaption v-if="props.block.content.caption || props.block.content.credits" class="section-caption small">
@@ -24,14 +41,5 @@
 		block: Block
 	}>();
 
-	const getEmbedUrl = (url: string) => {
-		const videoId = url.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i);
-
-		if (videoId && videoId[1]) {
-			return `https://www.youtube.com/embed/${videoId[1]}`;
-		}
-		return null;
-	};
-
-	const embedUrl = computed(() => getEmbedUrl(props.block.content.url));
+	const embed = computed(() => props.block.content.url ?? null);
 </script>
